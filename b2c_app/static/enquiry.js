a9e5e49d8b7a61615623cb7a9fa142df5c025a62
adnanhmd00 <adnanhmd000@gmail.com>
2019-12-01 10:09:00 +0530

    enquiry form working

--- b2c_app/static/enquiry.js
+++ b2c_app/static/enquiry.js
@@ -12,3 +12,40 @@ $scope.hotelSubmit = function(){
     $http.post('http://partner.thetravelsquare.in/api/v1/insert/hotel_enquiry.php', $scope.hotel).success
 };
 });
+
+
+app.controller('flightEnqCtrl', function($scope,$http){
+    $scope.flight = {};
+
+$scope.flightSubmit = function(){
+    console.log("Submitted");
+    console.log($scope.flight);
+
+
+    $http.post('http://partner.thetravelsquare.in/api/v1/insert/flight_enquiry.php', $scope.flight).success
+    };
+    });
+
+app.controller('cabEnqCtrl', function($scope,$http){
+    $scope.cabs = {};
+
+$scope.cabSubmit = function(){
+    console.log("Submitted");
+    console.log($scope.cabs);
+
+
+$http.post('http://partner.thetravelsquare.in/api/v1/insert/cab_enquiry.php', $scope.cabs).success
+};
+});
+
+app.controller('packageEnqCtrl', function($scope,$http){
+    $scope.package = {};
+
+$scope.packageSubmit = function(){
+    console.log("Submitted");
+    console.log($scope.package);
+
+
+$http.post('http://partner.thetravelsquare.in/api/v1/insert/package_enquiry.php', $scope.package).success
+};
+});
